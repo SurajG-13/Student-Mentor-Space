3a34cc622736f8c80e32eb226cd9f8c57e498c02
SurajG-13 <suraj.ghosh.2611@gmail.com>
2025-06-21 16:36:00 +0530

    Network Permission Changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,37 @@
-// require("dotenv").config({ path: "./env" });
+// import connectDB from "./db/index.js";
+// import dotenv from "dotenv";
+// import { app } from "./app.js";
+
+// dotenv.config({
+//   path: "./.env",
+// });
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log(`Server is Running at Port: ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(`Express Connection Error`, err);
+//   });
 
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 import { app } from "./app.js";
 
 dotenv.config({
-  path: "./.env",
+   path: "./.env",
 });
 
+const PORT = process.env.PORT || 5000;
+
 connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server is Running at Port: ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(`Express Connection Error`, err);
-  });
+   .then(() => {
+      app.listen(PORT, "0.0.0.0", () => {
+         console.log(`✅ Server is running at http://localhost:${PORT}`);
+      });
+   })
+   .catch((err) => {
+      console.log("Express Connection Error:", err);
+   });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,33 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//    plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+   server: {
+      host: true, // equivalent to '0.0.0.0'
+      port: 7000,
+   },
    plugins: [react()],
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// export default defineConfig({
-//    plugins: [react()],
-//    resolve: {
-//       alias: {
-//          "@backend": path.resolve(__dirname, "backend"), // Use alias to check which files are being bundled
-//       },
-//    },
-//    optimizeDeps: {
-//       exclude: ["@mapbox/node-pre-gyp", "aws-sdk", "mock-aws-s3", "nock"],
-//    },
-//    build: {
-//       rollupOptions: {
-//          output: {
-//             onwarn(warning, warn) {
-//                console.log("Warning: ", warning); // Log all warnings during the build
-//                warn(warning); // Ensure warnings are still shown
-//             },
-//          },
-//       },
-//    },
-// });
